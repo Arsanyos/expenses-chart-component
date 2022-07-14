95d315dcf7adbf5decf08aa993dd474154772718
Arsanyos <arsanyos1600@gmail.com>
2022-07-14 20:27:39 +0300

    chart comp structure setup

---
+++ src/components/Chart.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Chart() {
+  return (
+    <div className="chart-container">
+      <div className="chart-header"></div>
+      <div className="expenss-component"></div>
+    </div>
+  );
+}
+export default Chart;
