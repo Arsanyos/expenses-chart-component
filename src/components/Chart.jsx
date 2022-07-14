3c4fc067e469e165bc30d97ded8ff301b84a5885
Arsanyos <arsanyos1600@gmail.com>
2022-07-14 21:08:51 +0300

    chart header completed

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,9 +1,17 @@
 import React from "react";
-
+import logo from "../assests/logo.svg";
 function Chart() {
   return (
     <div className="chart-container">
-      <div className="chart-header"></div>
+      <div className="chart-header">
+        <div className="balance-container">
+            <h5>My balance</h5>
+            <h2>$921.48</h2>
+        </div>
+        <div className="logo-container">
+            <img className="logo" src={logo} alt="logo" height="50px" width="50px"/>
+        </div>
+      </div>
       <div className="expenss-component"></div>
     </div>
   );
